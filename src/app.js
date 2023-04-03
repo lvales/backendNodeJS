c91b388364b27366ab810f2f2a73fbb3fa9deea5
Luciano Vales <lvales73@gmail.com>
2023-04-02 21:23:03 -0300

    Desafio_clase_7

--- src/app.js
+++ src/app.js
@@ -16,28 +16,27 @@ app.listen(PORT, () => {
 app.get('/api/products', async (req, res) => {
 
    const limit = req.query.limit;
-   const idProduct = req.query.pid;
+   const products = await manager.getProducts();
 
    if (limit) {
-      const products = await manager.getProducts();
       const productsLimit = products.slice(0, limit);
       return res.send({
          products: productsLimit
       })
    }
+   return res.send({
+      products: products
+   })
+});
 
+app.get('/api/products/:id', async( req, res ) => {
+   
+   const idProduct = req.params.id;
+   
    if (idProduct) {
       const product = await manager.getProductById(idProduct);
       return res.send({
-         product
+         products: product
       })
    }
-
-   const products = await manager.getProducts();
-   return res.send({
-      products
-   })
-
 });
-
-

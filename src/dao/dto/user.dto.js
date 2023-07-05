2ec2dbedd89521538ba437f54c01fe771bbace90
Luciano Vales <lvales73@gmail.com>
2023-07-05 07:43:02 -0300

    dto

--- src/dao/dto/user.dto.js
+++ src/dao/dto/user.dto.js
@@ -1,24 +1,21 @@
-
-
-export class GetUser {
-      constructor(first_name, last_name, email, cartId, createdAt) {
-         this.first_name = first_name;
-         this.last_name = last_name;
-         this.email = email;
-         this.cartId = cartId;
-         this.createdAt = createdAt;
-      }
+export class CreateUser{
+   constructor(user){
+      this.first_name = user.first_name;
+      this.last_name = user.last_name;
+      this.email = user.email;
+      this.password = user.password;
+      this.age = user.age;
+      this.cartId = user.cartId;
+      this.rol = user.rol;
    }
+}
 
-   en session.routes.js 
-router.get("/current", userController.current);
-
-
-
-
-y el controller 
-current = (req, res) => {
-    let { first_name, last_name, email, age } = req.session.user;
-    const user = new CreateUserDto({ first_name, last_name, email, age });
-    res.send({ status: "success", payload: user });
-  };
+export class GetUser{
+   constructor(userDB){
+      this.first_name = userDB.first_name;
+      this.last_name = userDB.last_name;
+      this.email = userDB.email;
+      this.cartId = userDB.cartId;
+      this.rol = userDB.rol;
+   }
+}
